a162fa27e6f2c90bf1d11656fd221930dcc2f210
Ramon Olea <50149082+Ramon-Olea@users.noreply.github.com>
2023-01-03 14:33:08 -0600

    carga

---
+++ MVCCRUD/MVCCRUD/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace MVCCRUD.Models;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public DateTime? Fecha { get; set; }
+
+    public string? Clave { get; set; }
+}
